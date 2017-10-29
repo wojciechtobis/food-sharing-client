ea3f45906a19d9f0999426b307abf897921d4a7e
slivka- <slivka104@gmail.com>
2017-10-29 03:59:18 +0100

    Added order confirmation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,21 @@ import { AppComponent } from './app.component';
 import { BasicService } from './common/services/basic.service';
 import { HttpModule } from '@angular/http';
 import { HomeComponent } from './common/components/home/home.component';
+import { NewOfferConfirmComponent } from './common/components/new-offer-confirm/new-offer-confirm.component';
 import { HomeService } from './common/services/home.service';
 import { Routes, RouterModule } from '@angular/router';
 import { NewOfferComponent } from './common/components/new-offer/new-offer.component';
 import { NewOfferService } from './common/services/new-offer.service';
 import { ReactiveFormsModule } from '@angular/forms';
 import { OfferComponent } from './common/components/offer/offer.component';
+import { NewOrderConfirmComponent } from './common/components/new-order-confirm/new-order-confirm.component';
 
 const appRoutes: Routes = [
   {path: '', redirectTo: 'home', pathMatch: 'full'},
   { path: 'home', component: HomeComponent },
   { path: 'new-offer', component: NewOfferComponent },
-  { path: 'new-offer-confirm', component: NewOfferConfirmComponent}
+  { path: 'new-offer-confirm', component: NewOfferConfirmComponent},
+  { path: 'new-order-confirm', component: NewOrderConfirmComponent}
 ];
 
 @NgModule({
@@ -24,7 +27,8 @@ const appRoutes: Routes = [
     AppComponent,
     HomeComponent,
     NewOfferComponent,
-    OfferComponent
+    OfferComponent,
+    NewOrderConfirmComponent
   ],
   imports: [
     RouterModule.forRoot(appRoutes),
